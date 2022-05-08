b32cbed54191a9966f8df19bc05a309916a871d6
ot <tu.o.95@g.c>
2022-05-08 02:26:30 +0200

    it is merged

--- frontend/src/ProtectedRoutes.js
+++ frontend/src/ProtectedRoutes.js
@@ -1,18 +1,18 @@
 import React from 'react'
 import auth from "app_component/authentication/auth";
-import { useHistory } from "react-router";
- const ProtectedRoutes= ({children})=> {
-    const history = useHistory();
-    const navigateToLogin = () => {
-        history.push("/login");
-    }
-  
-    return(<>
-        {
-            auth.isAuthenticated() ?  children : navigateToLogin()
-        }
-    </>)
+import {useHistory} from "react-router";
 
+const ProtectedRoutes = ({children}) => {
+  const history = useHistory();
+  const navigateToLogin = () => {
+    history.push("/login");
+  }
+
+  return (<>
+    {
+      auth.isAuthenticated() ? children : navigateToLogin()
+    }
+    </>)
 }
 
 export default ProtectedRoutes
